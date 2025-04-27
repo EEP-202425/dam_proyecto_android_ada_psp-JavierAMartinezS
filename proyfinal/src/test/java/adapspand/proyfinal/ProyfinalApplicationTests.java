a08c58abb03d15112bd11b59916f9a29f8ad054b
JavierAMartinezS <Javier-Martinez1@Eep-Igroup.Com>
2025-04-27 17:39:43 +0200

    Proyecto importado de Spring Initializr

---
+++ proyfinal/src/test/java/adapspand/proyfinal/ProyfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package adapspand.proyfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
