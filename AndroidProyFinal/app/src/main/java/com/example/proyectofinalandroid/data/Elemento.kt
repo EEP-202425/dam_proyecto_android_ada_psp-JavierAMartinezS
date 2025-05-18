77718f1820860f9fdaf02ad55f1f780bccb66965
JavierAMartinezS <Javier-Martinez1@Eep-Igroup.Com>
2025-05-18 18:16:46 +0200

    Eliminando el proyecto obsoleto

--- AndroidProyFinal/app/src/main/java/com/example/proyectofinalandroid/data/Elemento.kt
+++
@@ -1,3 +0,0 @@
-package com.example.proyectofinalandroid.data
-
-data class Elemento(val nombre: String)
--- AndroidProyFinal/app/src/main/java/com/example/proyectofinalandroid/data/model/Billete.kt
+++
@@ -1,8 +0,0 @@
-package com.example.proyectofinalandroid.data.model
-
-import com.google.gson.annotations.SerializedName
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Billete(@SerializedName("id")
-                   val id: Long,)
